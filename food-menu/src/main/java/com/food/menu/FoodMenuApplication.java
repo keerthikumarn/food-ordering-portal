e652584818a1d876a72e7a8aab7f3518fc840513
Keerthi Kumar <n.keerthikumar85@gmail.com>
2025-04-08 21:34:21 +0530

    added the client class

--- food-menu/src/main/java/com/food/menu/FoodMenuApplication.java
+++ food-menu/src/main/java/com/food/menu/FoodMenuApplication.java
@@ -2,7 +2,10 @@ package com.food.menu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.food.menu.*"})
@@ -10,6 +13,12 @@ public class FoodMenuApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodMenuApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 	/*@GetMapping("/hi")
