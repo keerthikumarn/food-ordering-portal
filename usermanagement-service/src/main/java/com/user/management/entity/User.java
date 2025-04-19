dedcdfab8cdef32ec5b3d9186d357906cc60617a
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-04-19 15:37:11 +0530

    included the jpa repo interface for user entity

--- usermanagement-service/src/main/java/com/user/management/entity/User.java
+++ usermanagement-service/src/main/java/com/user/management/entity/User.java
@@ -1,5 +1,24 @@
 package com.user.management.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String username;
+	private String password;
+	private String address;
+	private String city;
 }
