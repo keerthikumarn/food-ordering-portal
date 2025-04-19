d580bb7bf708c19fbe9a82d04cb2831069c83070
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-04-19 15:20:20 +0530

    updated the controller class

--- restaurant-listing/src/main/java/com/restaurant/listing/controller/RestaurantListingController.java
+++ restaurant-listing/src/main/java/com/restaurant/listing/controller/RestaurantListingController.java
@@ -1,8 +1,44 @@
 package com.restaurant.listing.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restaurant.listing.dto.RestaurantDTO;
+import com.restaurant.listing.service.RestaurantService;
+
 @RestController
+@RequestMapping("/restaurant")
+@CrossOrigin
 public class RestaurantListingController {
 
+	@Autowired
+	private RestaurantService restaurantService;
+
+	@GetMapping("/getAllRestaurants")
+	public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants() {
+		List<RestaurantDTO> restaurantsList = restaurantService.findAllRestaurants();
+		return new ResponseEntity<>(restaurantsList, HttpStatus.OK);
+	}
+
+	@PostMapping("/addRestaurant")
+	public ResponseEntity<RestaurantDTO> saveRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
+		RestaurantDTO restaurant = restaurantService.saveRestaurant(restaurantDTO);
+		return new ResponseEntity<>(restaurant, HttpStatus.CREATED);
+	}
+
+	@GetMapping("/fetchById/{id}")
+	public ResponseEntity<RestaurantDTO> findRestaurantById(@PathVariable Integer id) {
+		RestaurantDTO restaurantDTO = restaurantService.fetchRestaurantById(id);
+		return new ResponseEntity<>(restaurantDTO, HttpStatus.OK);
+	}
 }
