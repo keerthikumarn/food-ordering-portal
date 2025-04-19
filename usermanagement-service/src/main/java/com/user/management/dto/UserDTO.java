81cdb0294142e72f6f3af871a923417ff5a1ac90
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-04-19 15:23:45 +0530

    implemented the DTO class for user

---
+++ usermanagement-service/src/main/java/com/user/management/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.user.management.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+	
+	private int id;
+    private String username;
+    private String password;
+    private String address;
+    private String city;
+
+}
