b9e1bfb7ddf6ef056c8630d00b2f030ce0b231c8
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-04-19 15:20:37 +0530

    started with the user management service

---
+++ usermanagement-service/src/main/java/com/user/management/UsermanagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.user.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementServiceApplication.class, args);
+	}
+
+}
