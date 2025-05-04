bfcf0b83d41e828fdd902a8581a2f6d9456a191b
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-05-04 16:34:58 +0530

    included the RestTemplate as a bean

--- order-management/src/main/java/com/order/management/OrderManagementApplication.java
+++ order-management/src/main/java/com/order/management/OrderManagementApplication.java
@@ -2,12 +2,21 @@ package com.order.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class OrderManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderManagementApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
