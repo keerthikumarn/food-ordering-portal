0f538782076bea7f9701bd93195591c7d5479b96
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-05-11 20:02:19 +0530

    performed some changes to mapper related

--- order-management/src/main/java/com/order/management/entity/Sequence.java
+++ order-management/src/main/java/com/order/management/entity/Sequence.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(collation = "sequence")
+@Document(collection = "sequence")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
--- order-management/src/main/java/com/order/management/mapper/OrderMapper.java
+++ order-management/src/main/java/com/order/management/mapper/OrderMapper.java
@@ -1,10 +1,12 @@
 package com.order.management.mapper;
 
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 import com.order.management.dto.OrderDTO;
 import com.order.management.entity.Order;
 
+@Mapper
 public interface OrderMapper {
 
 	OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
