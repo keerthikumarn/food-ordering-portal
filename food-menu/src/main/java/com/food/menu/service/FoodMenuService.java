aff0159dda82f00ad294e3418ff01404cce64bae
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-06-05 16:04:00 +0530

    included loggers

--- food-menu/src/main/java/com/food/menu/service/FoodMenuService.java
+++ food-menu/src/main/java/com/food/menu/service/FoodMenuService.java
@@ -13,7 +13,10 @@ import com.food.menu.entity.FoodItem;
 import com.food.menu.mapper.FoodItemMapper;
 import com.food.menu.repo.FoodItemRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class FoodMenuService {
 
 	@Autowired
@@ -23,17 +26,28 @@ public class FoodMenuService {
 	private RestTemplate restTemplate;
 
 	public FoodItemDTO addFoodItem(FoodItemDTO foodItemDTO) {
+		log.info("FoodMenuService - addFoodItem() called for food item: {}", foodItemDTO.getFoodItemName());
 		FoodItem foodItem = foodItemRepository.save(FoodItemMapper.INSTANCE.mapToFoodItemDTOToFoodItem(foodItemDTO));
+		log.info("FoodMenuService - Food item saved with ID: {}", foodItem.getId());
 		return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodItem);
 	}
 
 	public FoodMenuDTO fetchFoodCataloguePageDetails(Integer restaurantId) {
+		log.info("FoodMenuService - fetchFoodCataloguePageDetails() called with restaurantId: {}", restaurantId);
 		List<FoodItem> foodItemList = fetchFoodItemList(restaurantId);
+		log.info("FoodMenuService - Found {} food items for restaurantId: {}",
+				foodItemList == null ? 0 : foodItemList.size(), restaurantId);
 		RestaurantDTO restaurant = fetchRestaurantDetailsFromRestaurantMS(restaurantId);
+		if (restaurant == null) {
+			log.warn("FoodMenuService - No restaurant details found for restaurantId: {}", restaurantId);
+		} else {
+			log.info("FoodMenuService - Retrieved restaurant details for restaurantId: {}", restaurantId);
+		}
 		return createFoodMenuDTO(foodItemList, restaurant);
 	}
 
 	private FoodMenuDTO createFoodMenuDTO(List<FoodItem> foodItemList, RestaurantDTO restaurant) {
+		log.debug("FoodMenuService - Creating FoodMenuDTO");
 		FoodMenuDTO foodCataloguePage = new FoodMenuDTO();
 		foodCataloguePage.setFoodItemsList(foodItemList);
 		foodCataloguePage.setRestaurantDTO(restaurant);
@@ -41,12 +55,24 @@ public class FoodMenuService {
 	}
 
 	private RestaurantDTO fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
-		return restTemplate.getForObject("http://RESTAURANT-LISTING-SERVICE/restaurant/fetchById/" + restaurantId,
-				RestaurantDTO.class);
+		try {
+			log.debug("FoodMenuService - Calling Restaurant MS for restaurantId: {}", restaurantId);
+			return restTemplate.getForObject("http://RESTAURANT-LISTING-SERVICE/restaurant/fetchById/" + restaurantId,
+					RestaurantDTO.class);
+		} catch (Exception ex) {
+			log.error("FoodMenuService - Error fetching restaurant details for restaurantId: {} - {}", restaurantId,
+					ex.getMessage(), ex);
+			return null;
+		}
 	}
 
 	private List<FoodItem> fetchFoodItemList(Integer restaurantId) {
-		return foodItemRepository.findByRestaurantId(restaurantId);
+		log.debug("FoodMenuService - Fetching food items for restaurantId: {}", restaurantId);
+		List<FoodItem> foodItems = foodItemRepository.findByRestaurantId(restaurantId);
+		if (foodItems == null || foodItems.isEmpty()) {
+			log.warn("FoodMenuService - No food items found for restaurantId: {}", restaurantId);
+		}
+		return foodItems;
 	}
 
 }
