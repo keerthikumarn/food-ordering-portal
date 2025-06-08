a5d306feeaec4b3711cea7f5b0a08f667b2ace7f
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-06-08 16:02:20 +0530

    implemented logger in restaurant listing service

--- restaurant-listing/src/main/java/com/restaurant/listing/service/impl/RestaurantServiceImpl.java
+++ restaurant-listing/src/main/java/com/restaurant/listing/service/impl/RestaurantServiceImpl.java
@@ -13,6 +13,9 @@ import com.restaurant.listing.mapper.RestaurantMapper;
 import com.restaurant.listing.repo.RestaurantRepository;
 import com.restaurant.listing.service.RestaurantService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
 
@@ -21,30 +24,42 @@ public class RestaurantServiceImpl implements RestaurantService {
 
 	@Override
 	public List<RestaurantDTO> findAllRestaurants() {
+		log.info("RestaurantService - findAllRestaurants() called");
 		List<Restaurant> restaurantsList = restaurantRepo.findAll();
+		if (restaurantsList == null || restaurantsList.isEmpty()) {
+			log.warn("RestaurantServiceImpl - No restaurants found in the database");
+			return null;
+		}
 		List<RestaurantDTO> restaurantDtoList = null;
 		if (restaurantsList != null && !restaurantsList.isEmpty()) {
 			restaurantDtoList = restaurantsList.stream()
 					.map(restaurant -> RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant))
 					.collect(Collectors.toList());
 		}
+		log.info("RestaurantServiceImpl - Found {} restaurants", restaurantDtoList.size());
 		return restaurantDtoList;
 	}
 
 	@Override
 	public RestaurantDTO saveRestaurant(RestaurantDTO restaurantDTO) {
+		log.info("RestaurantServiceImpl - saveRestaurant() called for restaurant: {}", restaurantDTO.getName());
 		Restaurant restaurant = restaurantRepo
 				.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
+		log.info("RestaurantServiceImpl - Restaurant saved with ID: {}", restaurant.getId());
 		return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant);
 	}
 
 	@Override
 	public RestaurantDTO fetchRestaurantById(Integer id) {
+		log.info("RestaurantServiceImpl - fetchRestaurantById() called for ID: {}", id);
 		Optional<Restaurant> restaurant = restaurantRepo.findById(id);
 		if (restaurant.isPresent()) {
+			log.info("RestaurantService - Restaurant found for ID: {}", id);
 			return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(restaurant.get());
+		} else {
+			log.warn("RestaurantService - No restaurant found for ID: {}", id);
+			return null;
 		}
-		return null;
 	}
 
 }
