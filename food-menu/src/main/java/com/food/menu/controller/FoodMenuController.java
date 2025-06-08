36b6d2064a1aa444af6598015d8e5b307cd8ba0e
n.keerthikumar85@gmail.com <n.keerthikumar85@gmail.com>
2025-06-08 16:07:16 +0530

    included loggers in food menu controller

--- food-menu/src/main/java/com/food/menu/controller/FoodMenuController.java
+++ food-menu/src/main/java/com/food/menu/controller/FoodMenuController.java
@@ -3,6 +3,7 @@ package com.food.menu.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.food.menu.dto.FoodItemDTO;
 import com.food.menu.dto.FoodMenuDTO;
 import com.food.menu.service.FoodMenuService;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * This class is used to handle all the requests related to food menu.
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/foodmenu")
+@Slf4j
 public class FoodMenuController {
 
 	@Autowired
@@ -28,15 +31,22 @@ public class FoodMenuController {
 
 	@PostMapping("/addFoodItem")
 	public ResponseEntity<FoodItemDTO> addFoodItem(@RequestBody FoodItemDTO foodItemDTO) {
+		log.info("FoodMenuController - addFoodItem() invoked");
 		FoodItemDTO foodItemSaved = foodMenuService.addFoodItem(foodItemDTO);
+		log.info("FoodMenuController - addFoodItem() successfully saved food item with ID: {}", foodItemSaved.getId());
 		return new ResponseEntity<>(foodItemSaved, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/fetchRestaurantAndFoodItemsById/{restaurantId}")
 	public ResponseEntity<FoodMenuDTO> fetchRestauDetailsWithFoodMenu(@PathVariable Integer restaurantId) {
+		log.info("RestaurantController - fetchRestauDetailsWithFoodMenu() called with restaurantId: {}", restaurantId);
 		FoodMenuDTO foodCataloguePage = foodMenuService.fetchFoodCataloguePageDetails(restaurantId);
+		if (foodCataloguePage == null) {
+			log.warn("RestaurantController - No food menu found for restaurantId: {}", restaurantId);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		log.info("RestaurantController - Successfully retrieved food menu for restaurantId: {}", restaurantId);
 		return new ResponseEntity<>(foodCataloguePage, HttpStatus.OK);
-
 	}
 
 }
